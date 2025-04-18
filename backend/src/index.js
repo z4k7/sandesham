60a478a64cf882b154e0965b37a029ddbc022861
Sachin Kizhakkepurath <sachinkp1997@gmail.com>
2025-04-18 14:57:35 +0530

    fixed path issues

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
   app.get("*",(req,res)=>{
-    res.sendFile(path,join(__dirname,"../frontend","dist","index.html"))
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
   })
 }
 
