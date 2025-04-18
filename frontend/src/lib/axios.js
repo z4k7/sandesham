fe9a140e1412c63b7219a434de82fb64173d861b
Sachin Kizhakkepurath <sachinkp1997@gmail.com>
2025-04-18 14:50:53 +0530

    Axios formatted

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:5001/api"
-      : "/api",
+    import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
